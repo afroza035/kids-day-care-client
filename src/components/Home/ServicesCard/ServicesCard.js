1fd85e0e04fe6c151f53a09b3b0ffe0ecc7d6b5d
Afroza Akter Ruma <afroza.akter.ruma035@gmail.com>
2021-04-18 20:27:17 +0600

    add css animation

--- src/components/Home/ServicesCard/ServicesCard.js
+++ src/components/Home/ServicesCard/ServicesCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './ServiceCard.css';
 import { Link } from "react-router-dom";
 
 const ServicesCard = ({ classes }) => {
@@ -6,7 +7,7 @@ const ServicesCard = ({ classes }) => {
   return (
     <div className="col-md-4 mt-3">
       <div className="card">
-        <Link style={{textDecoration:"none"}} to={`/dashboard/book/${classes._id}`}>
+        <Link className="service-card" style={{textDecoration:"none"}} to={`/dashboard/book/${classes._id}`}>
           <img src={classes.imageUrl} alt="" className="card-img-top" />
           <div className="card-body">
             <h5 className="card-tittle">{classes.tittle}</h5>
